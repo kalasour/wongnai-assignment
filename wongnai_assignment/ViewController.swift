e31b36cb779d698f31f6e89d5fe086435d3e88ce
kalasour <nuntawat_wisedsup@cmu.ac.th>
2019-11-07 16:59:12 +0700

    Initial Commit

---
+++ wongnai_assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wongnai_assignment
+//
+//  Created by Nuntawat. Wisedsup on 7/11/2562 BE.
+//  Copyright © 2562 Nuntawat. Wisedsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
