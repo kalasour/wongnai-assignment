1c7b179e44d2eca3d2f5e97cef800b9601a4bd89
kalasour <nuntawat_wisedsup@cmu.ac.th>
2019-11-07 17:26:45 +0700

    init Datamodel and import asset to use

---
+++ wongnai_assignment/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  wongnai_assignment
+//
+//  Created by Nuntawat. Wisedsup on 7/11/2562 BE.
+//  Copyright © 2562 Nuntawat. Wisedsup. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
